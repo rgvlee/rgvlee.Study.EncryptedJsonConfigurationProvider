31582754e6b102eab241bcdd6ff45bf2943d4e66
rgvlee <lee@rgvlee.com>
2021-10-12 18:04:15 +0800

    Added support for a decryption password

--- rgvlee.Study.EncryptedJsonConfigurationProvider/Infrastructure/EncryptedJsonConfigurationProvider.cs
+++ rgvlee.Study.EncryptedJsonConfigurationProvider/Infrastructure/EncryptedJsonConfigurationProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.Extensions.Configuration.Json;
@@ -7,17 +8,36 @@ namespace rgvlee.Study.EncryptedJsonConfigurationProvider.Infrastructure
 {
     public class EncryptedJsonConfigurationProvider : JsonConfigurationProvider
     {
-        private const string encryptedValueKeyNameSuffix = ":Encrypted";
+        public static string ConfigurationDecryptionPasswordConfigurationKey = "Configuration:Decryption:Password";
+        public static string EncryptedValueKeyNameSuffix = ":Encrypted";
 
-        public EncryptedJsonConfigurationProvider(JsonConfigurationSource source) : base(source) { }
+        public EncryptedJsonConfigurationProvider(EncryptedJsonConfigurationSource source) : base(source) { }
+
+        private static bool TryGetConfigurationDecryptionPassword(IDictionary<string, string> data, out string password)
+        {
+            password = data.SingleOrDefault(x => x.Key.Equals(ConfigurationDecryptionPasswordConfigurationKey)).Value; //default KVP value is null
+            return data.ContainsKey(ConfigurationDecryptionPasswordConfigurationKey);
+        }
 
         public override void Load(Stream stream)
         {
+            Console.WriteLine($"Loading encrypted json configuration for '{Source.Path}'");
+
             base.Load(stream);
 
-            foreach (var kvp in Data.Where(x => x.Key.EndsWith(encryptedValueKeyNameSuffix, StringComparison.OrdinalIgnoreCase)).ToList())
+            if (!TryGetConfigurationDecryptionPassword(Data, out var password))
             {
-                Data.Add(kvp.Key.Substring(0, kvp.Key.Length - encryptedValueKeyNameSuffix.Length), string.Join(string.Empty, kvp.Value.Reverse()));
+                Console.WriteLine($"Configuration key '{ConfigurationDecryptionPasswordConfigurationKey}' not found.");
+            }
+
+            foreach (var kvp in Data.Where(x => x.Key.EndsWith(EncryptedValueKeyNameSuffix, StringComparison.OrdinalIgnoreCase)).ToList())
+            {
+                Console.WriteLine($"Decrypting '{kvp.Key}'");
+
+                Data.Add(
+                    kvp.Key.Substring(0, kvp.Key.Length - EncryptedValueKeyNameSuffix.Length),
+                    password.Equals("Decrypt") ? string.Join(string.Empty, kvp.Value.Reverse()) : "Denied"
+                );
                 Data.Remove(kvp.Key);
             }
         }
